9d33b979ec9f5552537e3ffcb6e3726ee7974c2c
Satoshi Ogata <satosystems@gmail.com>
2022-05-16 02:24:31 +0900

    Implement exerise No.1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import           Data.Char                      ( toUpper )
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  putStrLn no1
+
+q :: String
+q = "To be, or not to be: that is the question."
+
+no1 :: String
+no1 = unwords [ toUpper c : cs | (c : cs) <- words q ]
