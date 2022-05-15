c8f9e66dae4af181d2740e92a51961fe96a8b872
Satoshi Ogata <satosystems@gmail.com>
2022-05-16 02:37:32 +0900

    Implement exerise No.2

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,14 +2,28 @@ module Lib
   ( someFunc
   ) where
 
-import           Data.Char                      ( toUpper )
+import           Data.Char                      ( isAlphaNum
+                                                , isSpace
+                                                , toUpper
+                                                )
 
 someFunc :: IO ()
 someFunc = do
   putStrLn no1
+  putStrLn no2
 
 q :: String
 q = "To be, or not to be: that is the question."
 
 no1 :: String
 no1 = unwords [ toUpper c : cs | (c : cs) <- words q ]
+
+no2 :: String
+no2 = foldr f "" q
+ where
+  f cur acc
+    | isSpace cur = cur : acc
+    | isAlphaNum cur = cur : acc
+    | otherwise = case acc of
+      (' ' : cs) -> cur : '\n' : cs
+      _          -> cur : '\n' : acc
